2fcf507d6c490c1557333d5d83886eda43eafcaa
myfinds2026 <cck7rk47w4@privaterelay.appleid.com>
2026-09-16 15:19:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="threads-booklist-v1";
+const CACHE="threads-booklist-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
